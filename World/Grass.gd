01c3b60ad9863a796a6e1e98c49c31046cde324d
Almir Santos <almirpask@hotmail.com>
2020-06-12 00:07:08 -0300

    Add efficiently hitboxes using layers

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect : PackedScene = load("res://Effects/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()		
-		var world = get_tree().current_scene
-		grassEffect.global_position = global_position
-		world.add_child(grassEffect)
-		queue_free()
+func create_grass_effect():	
+	var GrassEffect : PackedScene = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()		
+	var world = get_tree().current_scene
+	grassEffect.global_position = global_position
+	world.add_child(grassEffect)
+	queue_free()
+
+
+func _on_Hurtbox_area_entered(area: Area2D) -> void:
+	create_grass_effect()
