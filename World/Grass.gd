df08900a264da5626941bad26d27dc6a996fe5a0
Almir Santos <almirpask@hotmail.com>
2020-06-15 01:24:52 -0300

    Reuse Effect Script add Bat death effect

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
-func create_grass_effect():	
-	var GrassEffect : PackedScene = load("res://Effects/GrassEffect.tscn")
-	var grassEffect = GrassEffect.instance()		
-	var world = get_tree().current_scene
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+func create_grass_effect():		
+	var grassEffect = GrassEffect.instance()
+	get_parent().add_child(grassEffect)	
 	grassEffect.global_position = global_position
-	world.add_child(grassEffect)
-	queue_free()
-
 
 func _on_Hurtbox_area_entered(_area: Area2D) -> void:
 	create_grass_effect()
+	queue_free()
