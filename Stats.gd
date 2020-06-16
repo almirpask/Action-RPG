11ed54bee3103f411784ad2ca46467f916d9bebb
Almir Santos <almirpask@hotmail.com>
2020-06-16 13:18:22 -0300

    create hearts UI

--- Stats.gd
+++ Stats.gd
@@ -1,14 +1,17 @@
 extends Node
 
-export(int) var max_health = 1
+export(int) var max_health = 1 setget set_max_health
 onready var health = max_health setget set_health
 
-func _ready() -> void:
-	print("Max health: " + str(max_health))
-	print("Health: " + str(health))
 signal no_health
+signal health_changed(value)
+signal max_health_changed(value)
 
+func set_max_health(value):
+	max_health = value
+	emit_signal("max_health_changed", max_health)
 func set_health(value):
-	health = value	
+	health = value
+	emit_signal("health_changed", health)
 	if health <= 0:
 		emit_signal("no_health")
